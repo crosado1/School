c8c34d3a1414e97c0775031d3fe9314c34610d67
unknown <Administrator@syslaptop63.mcs.local>
2016-06-27 11:19:14 -0400

    test Carlos

--- School/school.ui/Controllers/AccountController.cs
+++ School/school.ui/Controllers/AccountController.cs
@@ -18,7 +18,7 @@ namespace school.ui.Controllers
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
 
-        public AccountController()
+        public AccountControlle()
         {
         }
 
