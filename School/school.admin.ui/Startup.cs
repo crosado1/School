6ee1c1893e426d617ef1d2fdc9c208d8d23b2599
Carlos Rosado Santiago <cr2485@medicalcardsystem.com>
2016-08-22 16:34:50 -0400

    admin site

---
+++ School/school.admin.ui/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(school.admin.ui.Startup))]
+namespace school.admin.ui
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
