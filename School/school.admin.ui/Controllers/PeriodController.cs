8b732dde65c0c29610e1fc43e6de1454d35f8dbb
Carlos Rosado Santiago <cr2485@medicalcardsystem.com>
2016-08-23 13:27:39 -0400

    set layout page

---
+++ School/school.admin.ui/Controllers/PeriodController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace school.admin.ui.Controllers
+{
+    public class PeriodController : Controller
+    {
+        // GET: Period
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PeriodGrade()
+        {
+            return View();
+        }
+    }
+}
